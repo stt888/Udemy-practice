b3c0830d025ce991310a6ad4c0c53a40d0d15b86
tina <tinasun20@gmail.com>
2019-08-22 13:31:48 -0400

    change root direction

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <BrowserRouter>
           <Header/>
           <Switch>
-            <Route path = '/home' component = {Home}/>
+            <Route path = '/' component = {Home}/>
             <Route path = '/login' component = {Login}/>
             <Route path = '/register' component = {Register}/>
             <Route path = '/detail' component = {Detail}/>  
